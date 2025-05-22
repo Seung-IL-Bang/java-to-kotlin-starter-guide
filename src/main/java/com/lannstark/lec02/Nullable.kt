11c08985dccf07f8e7af89a8c8b6df03c0de08a9
lucky <lucky.s.bang@selvas.com>
2025-05-22 09:48:24 +0900

    Add functions to handle nullable strings in Nullable class

--- src/main/java/com/lannstark/lec02/Nullable.kt
+++ src/main/java/com/lannstark/lec02/Nullable.kt
@@ -1,4 +1,27 @@
 package com.lannstark.lec02
 
-class Nullable {
+fun main() {
+    val person = Person(null)
+    startsWith(person.name)
+}
+
+fun startsWith(str: String): Boolean {
+    return str.startsWith("abc")
+}
+
+fun startsWithA(str: String?): Boolean {
+    return str?.startsWith("A")
+        ?: throw IllegalArgumentException("str must not be null")
+}
+
+fun startsWithA2(str: String?): Boolean? {
+    return str?.startsWith("A")
+}
+
+fun startsWithA3(str: String?): Boolean {
+    return str?.startsWith("A") ?: false
+}
+
+fun startsWithA4(str: String?): Boolean {
+    return str?.startsWith("A") ?: false
 }
